89192dafa3ea0f83cf0fc4ddbebc47bac6bdce45
zhimaxu <zhimaxu@foxmail.com>
2026-06-14 02:20:38 +0800

    doc: 修改更多favicon相关

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -75,7 +75,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "assets/images/firefly.png",
+			value: "assets/images/favicon.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
